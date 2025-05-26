009eec4dbbf1461f6c7d6c18efcb990e114ecc45
Tulasi Pariyar <pariyartulasi766@gmail.com>
2025-05-26 15:12:50 +0545

    Create dell.js

---
+++ dell.js
@@ -0,0 +1,13 @@
+{
+  name: "Dell Technologies",
+  industry: "Technology",
+  score: 76,
+  leaks: [
+    {
+      issue: "E-waste takeback program below industry average",
+      impact: "$1.1M/year circularity gap",
+      solution: "Expand Dell Reconnect program globally",
+      source: "Dell Corporate Responsibility Update 2023"
+    }
+  ]
+}
