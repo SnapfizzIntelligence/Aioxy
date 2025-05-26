0274314765a1d4f13579b5bdc91caa29f3ab2f72
Tulasi Pariyar <pariyartulasi766@gmail.com>
2025-05-26 21:14:42 +0545

    Create heineken.js

---
+++ heineken.js
@@ -0,0 +1,13 @@
+export default {
+  name: "Heineken",
+  industry: "Food & Beverage",
+  score: 73,
+  leaks: [
+    {
+      issue: "Inconsistent emissions disclosures across regions",
+      impact: "$1.1M/year in ESG rating penalties",
+      solution: "Standardized global reporting framework (GRI or SASB)",
+      source: "[Heineken ESG Performance 2023](https://www.theheinekencompany.com/sites/default/files/2023-02/Heineken-Annual-Report-2022.pdf#page=106)"
+    }
+  ]
+}
