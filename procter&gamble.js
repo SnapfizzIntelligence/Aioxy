6236988b44f8ab2bcb1cd32efe05b091d02fbb62
Tulasi Pariyar <pariyartulasi766@gmail.com>
2025-05-26 14:15:59 +0545

    Create procter&gamble.js

---
+++ procter&gamble.js
@@ -0,0 +1,13 @@
+{
+  name: "Procter & Gamble",
+  industry: "FMCG",
+  score: 82,
+  leaks: [
+    {
+      issue: "Scope 3 emissions not fully disclosed",
+      impact: "$2.8M/year investor mistrust",
+      solution: "Add supplier GHG tracking w/ CDP Chain",
+      source: "P&G ESG Report 2023, pg. 18"
+    }
+  ]
+}
