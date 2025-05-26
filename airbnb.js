b7b2e41d9c44863a58bd9d9f3d130f797d0a8191
Tulasi Pariyar <pariyartulasi766@gmail.com>
2025-05-26 21:15:37 +0545

    Create airbnb.js

---
+++ airbnb.js
@@ -0,0 +1,13 @@
+export default {
+  name: "Airbnb",
+  industry: "Hospitality",
+  score: 70,
+  leaks: [
+    {
+      issue: "Carbon offset program not scaled globally",
+      impact: "$1.7M/year reputational exposure",
+      solution: "Partner with verified reforestation programs",
+      source: "[Airbnb ESG Report 2023](https://news.airbnb.com/wp-content/uploads/sites/4/2023/04/Airbnb_ESG_Report_2023.pdf#page=28)"
+    }
+  ]
+}
