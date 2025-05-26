f96877249dc57de62abf7fdd941a95db28bfcc3a
Tulasi Pariyar <pariyartulasi766@gmail.com>
2025-05-26 14:17:22 +0545

    Create hyndai.js

---
+++ hyndai.js
@@ -0,0 +1,13 @@
+{
+  name: "Hyundai",
+  industry: "Automotive",
+  score: 71,
+  leaks: [
+    {
+      issue: "EV production relies on non-renewable energy",
+      impact: "$1.7M/year carbon credit penalty (EU forecast)",
+      solution: "Shift 40% EV assembly to solar-backed plants",
+      source: "Hyundai ESG Report 2023, pg. 10"
+    }
+  ]
+}
