b0d4fc095c3eac14a31bbb780b8fb2db48e3abac
Tulasi Pariyar <pariyartulasi766@gmail.com>
2025-05-26 14:23:38 +0545

    Create nike.js

---
+++ nike.js
@@ -0,0 +1,13 @@
+{
+  name: "Nike",
+  industry: "Fashion",
+  score: 77,
+  leaks: [
+    {
+      issue: "No resale/refurbishment program (2023)",
+      impact: "$1.5M/year Gen Z brand detachment",
+      solution: "Scale Nike Refurbished initiative globally",
+      source: "Nike Impact Report 2023, pg. 20"
+    }
+  ]
+}
