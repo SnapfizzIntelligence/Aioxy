014540767533abae29c03dbda3e6468a5d1f476f
Tulasi Pariyar <pariyartulasi766@gmail.com>
2025-05-27 02:03:26 +0545

    Create vodafone.js

---
+++ vodafone.js
@@ -0,0 +1,19 @@
+export default {
+  name: "Vodafone",
+  industry: "Telecommunications",
+  score: 78,
+  leaks: [
+    {
+      issue: "No detailed Scope 3 emissions disclosure (2023)",
+      impact: "$2.4M/year EU carbon transparency risk",
+      solution: "Adopt CDP Tier 1 supplier disclosure by 2025",
+      source: "https://www.vodafone.com/sustainability/environment"
+    },
+    {
+      issue: "E-waste take-back rates below industry average",
+      impact: "$1.2M/year missed circularity opportunity",
+      solution: "Partner with NGOs + retailers for return infrastructure",
+      source: "https://www.vodafone.com/sites/default/files/2023-06/Sustainability-Report-2023.pdf"
+    }
+  ]
+}
