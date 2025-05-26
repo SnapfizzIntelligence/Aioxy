7423809ea8a687e92e1eae207192b43d40a2f673
Tulasi Pariyar <pariyartulasi766@gmail.com>
2025-05-26 15:19:55 +0545

    Create macdonald's.js

---
+++ macdonald's.js
@@ -0,0 +1,13 @@
+{
+  name: "McDonald's",
+  industry: "Hospitality",
+  score: 73,
+  leaks: [
+    {
+      issue: "Packaging not compostable in 40% of locations",
+      impact: "$1.7M/year sustainability penalty risk",
+      solution: "Adopt region-specific composting partners",
+      source: "McDonald's Sustainability Report 2023"
+    }
+  ]
+}
